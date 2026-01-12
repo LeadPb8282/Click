5084dfd18b6d683132582523ad1da446d2af74b5
LeadPb8282 <syw8183@naver.com>
2026-01-12 21:06:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const clickBtn = document.getElementById("clickBtn");
 const message = document.getElementById("message");
 
 const milestones = [
-  { value: 10, text: "10 cl: 당신은 이제야 기초적인 단계에 도달했다" },
-  { value: 50, text: "50 cl: 당신의 손가락은 이제야 만족 단계에 도달했다" },
-  { value: 100, text: "100 cl: 당신의 뇌는 이제 손가락을 컨트롤하는데 50% 사용한다" }
+  { value: 10, text: "10 cl에서 당신은 이제야 기초적인 단계에 도달했다" },
+  { value: 50, text: "50 cl에서 당신의 손가락은 이제야 만족 단계에 도달했다" },
+  { value: 100, text: "100 cl에서 당신의 뇌는 이제 손가락을 컨트롤하는데 50% 사용한다" }
 ];
 
 clickBtn.addEventListener("click", () => {
