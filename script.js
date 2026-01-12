bb4b1e367209e7d5e84b0f0c9f802862c6b5b9e8
LeadPb8282 <syw8183@naver.com>
2026-01-12 21:31:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ clickBtn.addEventListener("click", () => {
   const milestone = milestones.find(m => m.value === score);
   if (milestone) {
     message.textContent = milestone.text;
-    message.style.display = "block"; // milestone 도달하면 보이게
+    message.style.display = "inline-block";
   }
 });
